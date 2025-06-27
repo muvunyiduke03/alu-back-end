d57b7aa65b8c1d0eb35c13e42a29f84722b5e815
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-06-28 01:01:39 +0200

    Fix output message formatting to include total tasks in 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     done_tasks = [task for task in todos if task.get("completed")]
 
     print(
-        f"Employee {employee_name} is done with tasks({len(done_tasks)})"
+        f"Employee {employee_name} is done with tasks({len(done_tasks)}/{len(todos)}):"
         )
     for task in done_tasks:
         print(f"\t {task.get('title')}")
